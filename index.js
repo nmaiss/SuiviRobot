09b17b5abd270b3a87d80b85102b32e366ba9d38
nicolas <nicolas.maissouradze@gmail.com>
2021-08-15 07:30:31 +0200

    add express to package.json

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const { Telegraf, Markup, Context, session, Stage, WizardScene, Scenes } = require("telegraf");
 const { Keyboard, Key } = require('telegram-keyboard')
 const { CallbackData } = require('telegraf-callback-data');
+var express = require('express');
 
 const mongoose = require('mongoose');
+
+var app = express();
+app.set('port', (process.env.PORT || 5000));
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 require('dotenv').config();
 mongoose.connect(process.env.MONGO);
